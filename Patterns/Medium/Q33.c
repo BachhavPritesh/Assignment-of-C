173c917a3f071af7d94cc69c331704939a3e5229
BachhavPritesh <pritesh.v.bachhav.cg@gmail.com>
2025-09-17 11:24:11 +0530

    31 to 40

---
+++ Patterns/Medium/Q33.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    
+    int n;
+    scanf("%d", &n);
+    
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            
+            if(j==1||i==j||i==n){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        
+        printf("\n");
+    }
+    
+    
+    
+    
+    
+    return 0;
+}
